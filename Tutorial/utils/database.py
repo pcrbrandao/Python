2512416dbadf2b67a57159ae50f1c93812d2a529
Pedro Brandao <pcrbrandao@gmail.com>
2016-12-23 15:58:30 -0200

    Em desenvolvimento. Melhoramentos.

--- Tutorial/utils/database.py
+++ Tutorial/utils/database.py
@@ -2,23 +2,28 @@
 A Base está em domain pois as classes de dominio herdam dela."""
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, Session
+from sqlalchemy.engine import Engine
 
 # As classes devem ser importadas aqui para que o init_db possa criá-las.
 import domain
 
-engine = None
-db_session = None
 
-try:
-    engine = create_engine('mysql+pymysql://root:root@localhost:8889/receitas')
-    db_session = scoped_session(sessionmaker(autocommit=False,
-                                             autoflush=False,
-                                             bind=engine))
-except Exception as err:
-    print("Não pude conectar: {}".format(err))
-    engine = None
-    db_session = None
+def get_engine() -> Engine:
+    try:
+        return create_engine('mysql+pymysql://root:root@localhost:8889/receitas')
+
+    except Exception as err:
+        print("Não pude conectar: {}".format(err))
+        return err
+
+def get_db_session(engine: Engine) -> Session:
+    return scoped_session(sessionmaker(autocommit=False,
+                                       autoflush=False,
+                                       bind=engine))
+
+engine = get_engine()
+db_session = get_db_session(engine)
 
 def init_db():
     """O IDE não oferece o metadata no autocomplete, mas está funcionando
--- Tutorial/utils/test_user_dao.py
+++ Tutorial/utils/test_user_dao.py
@@ -10,19 +10,17 @@ class TestUserController(unittest.TestCase):
     def test_add_deve_retornar_none(self):
         """Acrescenta um usuário. Se não retornar erro está ok."""
 
-        us = Usuario(nome='Pedro', email='email', senha='1234')
-        result = usuario_dao.add(us)
+        usuario = Usuario(nome='Pedro', email='email', senha='1234')
+        result = usuario_dao.add(usuario)
+
+        # o usuário deve ser encontrado no db
+        usuario_db = usuario_dao.find("Pedro")
+        print("o usuário {} foi adicionado com sucesso!".format(usuario_db))
+
+        # vou deletar para deixar o db vazio
+        usuario_dao.delete(usuario)
 
         self.assertIsNone(result)
-
-    def test_find_deve_encontrar(self):
-        """Procura o usuario inserido no test_add"""
-
-        nome = "Pedro"
-        result = usuario_dao.find(nome=nome)
-        print(result)
-
-        self.assertEqual(result.nome, nome)
 
     def test_find_deve_retornar_none(self):
         """O usuário não existe no banco, por isso deve retornar None."""
@@ -30,6 +28,43 @@ class TestUserController(unittest.TestCase):
         result = usuario_dao.find(nome=nome)
 
         self.assertIsNone(result)
+
+    def test_delete_deve_retornar_none(self):
+        """Depois de adicionado o usuário existe, então ele pode ser excluído."""
+        entrada = Usuario(nome="Pedro", email="email", senha="1234")
+        usuario_dao.add(entrada)
+
+        usuario = usuario_dao.find("Pedro")
+        result = usuario_dao.delete(usuario)
+
+        self.assertIsNone(result)
+
+    def test_total_de_paginas_deve_ser_um(self):
+        """Se tem-se 10 registros e cada página tem 10, então tem-se 1 página"""
+        registros = 10
+        total = usuario_dao.total_de_paginas(registros, usuario_dao.usuarios_por_pagina())
+
+        self.assertEqual(total, 1)
+
+    def test_total_de_paginas_deve_ser_zero(self):
+        """Se não há registros o número de páginas é zero"""
+        registros = 0
+        total = usuario_dao.total_de_paginas(registros, usuario_dao.usuarios_por_pagina())
+
+        self.assertEqual(total, 0)
+
+    def test_total_deve_ser_um_para_menor_que_dez(self):
+        """Se registros é menor que dez e maior que zero o total é 1"""
+        registros = 3
+        total = usuario_dao.total_de_paginas(registros, usuario_dao.usuarios_por_pagina())
+
+        self.assertEqual(total, 1)
+
+    def test_total_deve_ser_dois_para_onze(self):
+        registros = 11
+        total = usuario_dao.total_de_paginas(registros, usuario_dao.usuarios_por_pagina())
+
+        self.assertEqual(total, 2)
 
 if __name__ == '__main__':
     unittest.main()
